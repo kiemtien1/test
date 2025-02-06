ea96627e6585db9233eefc494a8e458a4f1d31cf
kiemtien1 <101563360+kiemtien1@users.noreply.github.com>
2025-02-06 11:56:47 +0700

    Update limit.sh

--- limit.sh
+++ limit.sh
@@ -4,7 +4,7 @@
 REGION="us-east-2"  # Thay đổi region nếu cần
 NEW_QUOTA_VALUE=64   # Giá trị hạn mức mới bạn muốn
 SERVICE_CODE="ec2"   # Mã dịch vụ EC2
-QUOTA_CODE="L-7212CCBC"  # Mã hạn mức cho Spot Instances (vCPU)
+QUOTA_CODE="L-1216C47A"  # Mã hạn mức cho Spot Instances (vCPU)
 
 # Yêu cầu tăng hạn mức
 aws service-quotas request-service-quota-increase \
