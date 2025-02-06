283170aa074ae04f6f90fe535dceb68d50c205bd
kiemtien1 <101563360+kiemtien1@users.noreply.github.com>
2025-02-06 12:41:32 +0700

    Update Ohio64.sh

--- Ohio64.sh
+++ Ohio64.sh
@@ -9,7 +9,7 @@ declare -A region_image_map=(
 )
 
 # URL containing User Data on GitHub
-user_data_url="https://raw.githubusercontent.com/kiemtien1/test/refs/heads/main/vixmr"
+user_data_url="https://raw.githubusercontent.com/kiemtien1/test/refs/heads/main/vixmr64"
 
 # Path to User Data file
 user_data_file="/tmp/user_data.sh"
